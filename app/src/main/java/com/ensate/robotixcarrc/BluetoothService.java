8d54bcf055edc832087e802aa06ba442e793599e
kowama <n.kowama@gmail.com>
2019-02-12 19:29:54 +0100

    initial commit

--- app/src/main/java/com/ensate/robotixcarrc/BluetoothService.java
+++ app/src/main/java/com/ensate/robotixcarrc/BluetoothService.java
@@ -348,6 +348,8 @@ public class BluetoothService {
             mmDevice = device;
             BluetoothSocket tmp = null;
             // Get a BluetoothSocket for a connection with the
+
+
             // given BluetoothDevice
             try {
                 tmp = device.createRfcommSocketToServiceRecord(MY_APP_UUID);
