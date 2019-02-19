a077c263e841af88dc6977baef4316aa15e62af5
kowama <n.kowama@gmail.com>
2019-02-20 00:48:38 +0100

    help activity back button option added

--- app/src/main/java/com/ensate/robotixcarrc/MainActivity.java
+++ app/src/main/java/com/ensate/robotixcarrc/MainActivity.java
@@ -124,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
         // Performing this check in onResume() covers the case in which BT was
         // not enabled during onStart(), so we were paused to enable it...
         // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+
         if (mBluetoothService != null) {
             // Only if the state is STATE_NONE, do we know that we haven't started already
             if (mBluetoothService.getState() == mBluetoothService.STATE_NONE) {
