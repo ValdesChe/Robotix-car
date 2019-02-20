1846f698cc3acf0e668e2b0e1660d09875e29167
kowama <n.kowama@gmail.com>
2019-02-20 23:25:49 +0100

    App work on full screen

--- app/src/main/java/com/ensate/robotixcarrc/MainActivity.java
+++ app/src/main/java/com/ensate/robotixcarrc/MainActivity.java
@@ -5,11 +5,11 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private BluetoothService mBluetoothService;
 
+
     /**
      * control buttons
      */
@@ -65,14 +66,36 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mMoveUpButton;
     private ImageButton mMoveDownButton;
 
+    private View mDecorView;
+
+
     private boolean doubleBackToExitPressedOnce = false;
+
+    private View.OnTouchListener mControlsButtonOnTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            ImageButton imageButton = (ImageButton) v;
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                imageButton.setBackgroundColor(getResources().getColor(R.color.btn_bg_pressed));
+                // sendBluetoothData(button);
+                return true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                imageButton.setBackgroundColor(getResources().getColor(R.color.btn_bg_default));
+                return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // set view
+        mDecorView = getWindow().getDecorView();
+        // hide system ui
+        hideSystemUI();
+
         // Get local Bluetooth adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -83,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
         }
         //initMembers and setup control pad
         setupControlPadView();
-
     }
 
 
@@ -101,21 +123,21 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.connect_scan: {
                 openConnectActivity();
-                return true;
             }
             case R.id.connect_scan_menu_item: {
                 openConnectActivity();
-                return true;
             }
             case R.id.discoverable: {
                 // TODO: 2/13/19
-                return true;
+            }
+            case R.id.action_full_screen: {
+                hideSystemUI();
             }
             case R.id.action_help: {
                 openHelpActivity();
             }
         }
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -133,6 +155,12 @@ public class MainActivity extends AppCompatActivity {
                 mBluetoothService.start();
             }
         }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus)
+            hideSystemUI();
     }
 
 
@@ -154,32 +182,50 @@ public class MainActivity extends AppCompatActivity {
 
 
     /******========Private methods===========******/
+    /**
+     * hide SystemUI to make App full screen
+     */
+
+    private void hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            mDecorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+            );
+        }
+    }
+
+    /**
+     * showSystemUI
+     */
+    private void showSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            mDecorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+    }
 
 
     /**
      * setup Control pad View
      */
-
     private void setupControlPadView() {
 
         mMoveUpButton = findViewById(R.id.upButton);
-        mMoveUpButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                ImageButton imageButton = (ImageButton) v;
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    imageButton.setBackgroundColor(getResources().getColor(R.color.btn_bg_pressed));
-                    // sendBluetoothData(button);
-                    return true;
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    imageButton.setBackgroundColor(getResources().getColor(R.color.btn_bg_default));
-                    return true;
-                }
-                return false;
-            }
-        });
+        mMoveDownButton = findViewById(R.id.downButton);
+        mMoveLeftButton = findViewById(R.id.leftButton);
+        mMoveRightButton = findViewById(R.id.rightButton);
 
-
+        mMoveUpButton.setOnTouchListener(mControlsButtonOnTouchListener);
+        mMoveDownButton.setOnTouchListener(mControlsButtonOnTouchListener);
+        mMoveLeftButton.setOnTouchListener(mControlsButtonOnTouchListener);
+        mMoveRightButton.setOnTouchListener(mControlsButtonOnTouchListener);
     }
 
     private void openConnectActivity() {
